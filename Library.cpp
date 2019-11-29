c4222309b1c6f86e4e819d645ad972306a3b2fce
Milan280 <57249404+Milan280@users.noreply.github.com>
2019-11-29 06:57:35 -0800

    Update Library.cpp

--- Library.cpp
+++ Library.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    int age[5]={24,26,27,80,81};
+    age[3]=78;
+
+    for(i=0;i<5;i++){
+        cout <<age[i] << endl;
+    }
+    return 0;
+}
 
